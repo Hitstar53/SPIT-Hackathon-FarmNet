20ea7228574d63209fcae2e3fb7da85f7d1ee4f7
Vineet Parmar <vineet.parmar@spit.ac.in>
2024-02-11 00:57:29 +0530

    commit

--- app/home/Home.jsx
+++ app/home/Home.jsx
@@ -97,7 +97,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     justifyContent: "space-between",
-    backgroundColor: "#fff",
   },
   sqcontainer: {
     flex: 1,
